f10f138815fa84936564d2977c73da5b1505ec3c
Disilon <disilon@gmail.com>
2024-07-10 23:37:36 +0500

    Заменил в контроллере ссылку с реализации на интрефейс.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,23 +6,23 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.CarServiceImpl;
+import web.service.CarService;
 
 import java.util.List;
 
 @Controller
 public class CarController {
-    private final CarServiceImpl carServiceImpl;
+    private final CarService carService;
 
     @Autowired
-    public CarController(CarServiceImpl carServiceImpl) {
-        this.carServiceImpl = carServiceImpl;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(value = "/cars")
     public String getCars(@RequestParam(name = "count", required = false, defaultValue = "5") int count,
                           ModelMap model) {
-        List<Car> cars = carServiceImpl.getCars(count);
+        List<Car> cars = carService.getCars(count);
         model.addAttribute("cars", cars);
         return "cars";
     }
